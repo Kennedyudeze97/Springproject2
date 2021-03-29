8cd56661bec9349f3d4c57406097d891bb6ddd8a
Kennedyudeze97 <kennedyudeze@gmail.com>
2021-03-29 11:18:01 +0300

    Changes

--- config.py
+++ config.py
@@ -8,5 +8,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'yourwillneverknowme'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite:///' + os.path.join(basedir, 'app.db')  or \
+            'mysql + pymysql://<db_user>:<db_password>@<database-1.cwvtromjtobl.us-east-2.rds.amazonaws.com>/<>'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
